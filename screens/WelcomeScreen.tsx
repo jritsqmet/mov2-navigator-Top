15cb78546814593247baa2cb73ea66336b6cdbf4
unknown <jritsqmet@gmail.com>
2024-01-02 17:58:32 -0500

    Revert "CRUD FIREBASE"
    This reverts commit 5001eb7f8e60fd306431ceeef25b9f3ec8510bfb.

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -5,7 +5,7 @@ export default function WelcomeScreen({navigation}: any) {
   return (
     <View>
       <Text>Welcome</Text>
-      <Button title='Ir a Usuario' onPress={()=> navigation.navigate('Tabs')}/>
+      <Button title='Ir a Usuario' onPress={()=> navigation.navigate('Usuario')}/>
     </View>
   )
 }
